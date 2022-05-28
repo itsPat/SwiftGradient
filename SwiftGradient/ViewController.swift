0719b2330cce970303ca4593a235c47777b5d077
Pat Trudel <patrick.trudel@me.com>
2022-05-28 10:40:21 -0400

    Initial Commit

---
+++ SwiftGradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGradient
+//
+//  Created by Pat Trudel on 5/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
